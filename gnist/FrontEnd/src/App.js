eba5b691c241925316e7fa55444ab0ba4ef03c25
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-12 18:53:30 +0100

    Updated Auth0Provider with authorizationParams

--- gnist/FrontEnd/src/App.js
+++ gnist/FrontEnd/src/App.js
@@ -4,7 +4,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import HomePage from './components/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -14,13 +13,14 @@ function App() {
     <Auth0Provider
       domain={auth0Domain}
       clientId={auth0ClientId}
-      redirectUri={window.location.origin}
+      authorizationParams={{ redirect_uri: window.location.origin }} 
     >
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          {/* Protected routes will  be added here with the ProtectedRoute component */}
         </Routes>
       </Router>
     </Auth0Provider>
