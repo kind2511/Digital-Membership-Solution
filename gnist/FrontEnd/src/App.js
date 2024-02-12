a00cd34013c73d8e06aed598d2df4d5b2eb9e7ad
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-12 15:48:51 +0100

    Integrate Auth0 for secure authentication and setup protected routes

--- gnist/FrontEnd/src/App.js
+++ gnist/FrontEnd/src/App.js
@@ -4,6 +4,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import HomePage from './components/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
