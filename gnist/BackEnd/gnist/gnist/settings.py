f73df83d55bf5f6995c3bb8022bb5c095d0df53e
kind2511 <chrisa2511@gmail.com>
2024-02-12 22:16:51 +0100

    connected mysql to django

--- gnist/BackEnd/gnist/gnist/settings.py
+++ gnist/BackEnd/gnist/gnist/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "gnist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Gnist",
+	"USER": "root",
+	"PASSWORD": "Jdqo4tt9",
+	"PORT": 3306,
+	"HOST": "127.0.0.1",
     }
 }
 
