76d4b4e9f9e0aaeb55200d68f45afc2658b3df4b
kind2511 <chrisa2511@gmail.com>
2024-02-15 14:31:14 +0100

    changed db temporarely

--- gnist/BackEnd/gnist/gnist/settings.py
+++ gnist/BackEnd/gnist/gnist/settings.py
@@ -73,17 +73,23 @@ WSGI_APPLICATION = "gnist.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.mysql",
+#        "NAME": "gnist",
+#	"USER": "admin",
+#	"PASSWORD": "bg9uxDPNYXZzIuX5Fke8",
+#	"PORT": 3306,
+#	"HOST": "database-1.chiwoyka4rye.eu-north-1.rds.amazonaws.com",
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "gnist",
-	"USER": "admin",
-	"PASSWORD": "bg9uxDPNYXZzIuX5Fke8",
-	"PORT": 3306,
-	"HOST": "database-1.chiwoyka4rye.eu-north-1.rds.amazonaws.com",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
