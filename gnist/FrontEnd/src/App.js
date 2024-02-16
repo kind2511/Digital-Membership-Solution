e23d4445b104430134390b1ea6d6c798aa8d45d5
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-16 16:34:36 +0100

    Add UserDashboard and AdminDashboard components with no styling

--- gnist/FrontEnd/src/App.js
+++ gnist/FrontEnd/src/App.js
@@ -4,6 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import HomePage from './components/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import UserDashboard from './components/UserDashboard';
+import AdminDashboard from './components/AdminDashboard';
 
 function App() {
   const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -20,7 +22,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          {/* Protected routes will  be added here with the ProtectedRoute component */}
+          <Route path="/user-dashbaord"element={<UserDashboard />} />
+          <Route path="/user-dashbaord"element={<AdminDashboard />} />
         </Routes>
       </Router>
     </Auth0Provider>
---
+++ gnist/FrontEnd/src/components/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UserDashboard.css'; 
+
+
+function UserDashboard() {
+  return (
+    <div className="user-dashboard">
+      <h1>User Dashboard</h1>
+      {/* Dashboard content add later */}
+    </div>
+  );
+}
+
+export default UserDashboard;
