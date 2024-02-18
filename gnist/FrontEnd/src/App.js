099584c02fe6c56b95664f97136a77c7f6b3c70d
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-18 16:08:57 +0100

    Add logout functionality and logic to direct user to dashboard based on role

--- gnist/FrontEnd/src/App.js
+++ gnist/FrontEnd/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import HomePage from './components/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -15,15 +15,15 @@ function App() {
     <Auth0Provider
       domain={auth0Domain}
       clientId={auth0ClientId}
-      authorizationParams={{ redirect_uri: window.location.origin }} 
+      redirectUri={window.location.origin}
     >
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePageWithRedirection />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/user-dashbaord"element={<UserDashboard />} />
-          <Route path="/user-dashbaord"element={<AdminDashboard />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
         </Routes>
       </Router>
     </Auth0Provider>
@@ -31,3 +31,25 @@ function App() {
 }
 
 export default App;
+
+/*
+ *Redirects authenticated users to the appropriate dashboard based on their role.
+ */
+
+function HomePageWithRedirection() {
+  const { isAuthenticated, user, isLoading } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      const role = user['https://my-namespace/role']; // must be fixed 
+      if (role === 'admin') {
+        navigate('/admin-dashboard');
+      } else {
+        navigate('/user-dashboard');
+      }
+    }
+  }, [isAuthenticated, isLoading, navigate, user]);
+
+  return <HomePage />;
+}
