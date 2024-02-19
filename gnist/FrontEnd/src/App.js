fb969a7414b22d91e7c8e1e046b14c02c5a94c5e
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-19 22:03:44 +0100

    Move the auth0 Provider to index

--- gnist/FrontEnd/src/App.js
+++ gnist/FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import HomePage from './components/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -10,40 +10,24 @@ import Loading from './components/Loading';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
   return (
-    <Auth0Provider
-      domain={auth0Domain}
-      clientId={auth0ClientId}
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePageWithRedirection />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/user-dashboard" element={
-              <ProtectedRoute component={UserDashboard} />
-            } />
-          <Route path="/admin-dashboard" element={
-            <ProtectedRoute component={AdminDashboard} />
-          } />
-        </Routes>
-      </Router>
-    </Auth0Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePageWithRedirection />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/user-dashboard"
+          element={<ProtectedRoute component={UserDashboard} />}
+        />
+        <Route
+          path="/admin-dashboard"
+          element={<ProtectedRoute component={AdminDashboard} />}
+        />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
-
-/*
- *Redirects authenticated users to the appropriate dashboard based on their role.
- */
 
 function HomePageWithRedirection() {
   const { isAuthenticated, user, isLoading } = useAuth0();
@@ -51,7 +35,7 @@ function HomePageWithRedirection() {
 
   useEffect(() => {
     if (!isLoading && isAuthenticated) {
-      const role = user['https://my-namespace/role']; // must be fixed 
+      const role = user['https://my-namespace/role']; // will be fixed by RULES 
       if (role === 'admin') {
         navigate('/admin-dashboard');
       } else {
@@ -62,3 +46,5 @@ function HomePageWithRedirection() {
 
   return isLoading ? <Loading /> : <HomePage />;
 }
+
+export default App;
