b8804ebb9f3daa6628fb63082660414af99d8a9c
kind2511 <chrisa2511@gmail.com>
2024-02-21 15:16:31 +0100

    added some functions to the view

--- gnist/BackEnd/gnist/digital_medlemsordning/urls.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('get_all_members/', views.get_all_member_data, name='all_member_data'),
+    path('get_member/<int:user_id>/', views.get_one_member_data, name='member_data'),
+    path('ban_member/<int:user_id>/', views.ban_member, name='ban_member'),
 ]
+
--- gnist/BackEnd/gnist/digital_medlemsordning/views.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/views.py
@@ -1,8 +1,99 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, from the simplest view.")
+from django.http import JsonResponse
+import json
+from datetime import date
+from django.shortcuts import get_object_or_404
+from .models import Members
 
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, from the root path of digital_medlemsording.")
+
+
+def get_all_member_data(request):
+    members = Members.objects.all()
+    member_data = []
+
+    for member in members:
+        days_without_incident = member.days_without_incident
+        if days_without_incident <= 19:
+            level = "Noob"
+        elif 20 <= days_without_incident <= 49:
+            level = "Rookie"
+        elif 50 <= days_without_incident <= 99:
+            level = "Pro"
+        else:
+            level = "Legend"
+        
+        is_banned = member.banned
+        if is_banned == 0:
+            profile_color = "green"
+        elif is_banned == 1:
+            profile_color = "red"
+
+        member_info = {
+            'first_name': member.first_name,
+            'level': level,
+            'profile_color': profile_color 
+        }
+        member_data.append(member_info)
+    
+    today_date = date.today().strftime("%Y-%m-%d")
+
+    response_data = {
+        'date': today_date,
+        'members': member_data
+    }
+    return JsonResponse(response_data, safe=False)
+
+
+
+def get_one_member_data(request, user_id):
+    try:
+        member = Members.objects.get(userID=user_id)
+    except Members.DoesNotExist:
+        return JsonResponse({'error': 'User does not exist'}, status=404)
+
+    days_without_incident = member.days_without_incident
+    if days_without_incident <= 19:
+        level = "Noob"
+    elif 20 <= days_without_incident <= 49:
+        level = "Rookie"
+    elif 50 <= days_without_incident <= 99:
+        level = "Pro"
+    else:
+        level = "Legend"
+    
+    is_banned = member.banned
+    if is_banned:
+        profile_color = "red"
+    else:
+        profile_color = "green"
+
+    member_info = {
+        'first_name': member.first_name,
+        'level': level,
+        'profile_color': profile_color 
+    }
+    
+    today_date = date.today().strftime("%Y-%m-%d")
+
+    response_data = {
+        'date': today_date,
+        'member': member_info
+    }
+    return JsonResponse(response_data)
+
+def ban_member(request, user_id):
+    try:
+        member = Members.objects.get(userID=user_id)
+    except Members.DoesNotExist:
+        return JsonResponse({'error': 'User does not exist'}, status=404)
+
+    member.banned = True
+    member.save()
+
+    return JsonResponse({'message': 'Member banned successfully'})
