b6f7bd22941404d0f8379e865c8b0670a543f90d
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-27 17:50:43 +0100

    Inhance the desgin of main part of userDashboard & add function to fetch data from backend

--- gnist/FrontEnd/src/components/UserDashboard.js
+++ gnist/FrontEnd/src/components/UserDashboard.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 import './UserDashboard.css';
 
 function UserDashboard() {
   const { logout, user } = useAuth0();
   const date = new Date().toLocaleDateString();
+  const levelColors = ['noob', 'rookie', 'pro', 'legend'];
+  const userLevelIndex = levelColors.indexOf(user.level?.toLowerCase()) + 1;
+  const [memberData, setMemberData] = useState(null); 
 
   const handleLogout = () => {
     logout({ returnTo: window.location.origin });
   };
 
-    // "Lock" the current page in the browser's history
+  useEffect(() => {
+    // test to get member data
+    const fetchMemberData = async () => {
+      try {
+        const response = await axios.get('/api/member-data', { params: { userId: user.sub } });
+        setMemberData(response.data);
+      } catch (error) {
+        console.error('There was an error fetching the member data:', error);
+      }
+    };
+
+    if (user.sub) { // Only fetch member data if user.sub exist
+      fetchMemberData();
+    }
+  }, [user.sub]); // No  include setMemberData here
+
+  // "Lock" the current page in the browser's history
   window.history.pushState(null, document.title, window.location.href);
-  window.addEventListener('popstate', function(event) {
+  window.addEventListener('popstate', function (event) {
     window.history.pushState(null, document.title, window.location.href);
   });
 
   return (
     <div className="dashboard-container">
+      {/* Sidebar */}
       <div className="sidebar">
         <div className="sidebar-header">
           <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="sidebar-logo" />
@@ -30,17 +51,25 @@ function UserDashboard() {
           <div className="sidebar-item logout-item" onClick={handleLogout}>Logg Ut</div>
         </div>
       </div>
+
+      {/* Main Content- the white page in user dashboard */}
       <div className="main-content">
-        {/* User's Picture */}
+        {/* Users Picture */}
         {user.picture && <img src={user.picture} alt="User" className="user-picture" />}
-        
-        {/* User's First Name */}
-        {user.name && <h1>Welcome, {user.name.split(' ')[0]}!</h1>}
-        
-        {/* Today's Date */}
-        <p>Dato: {date}</p>
-        
-        {/* Rest of the dashboard content */}
+
+        {/* Welcome Message */}
+        {user.name && <h1 className="welcome-message">Velkommen {user.name.split(' ')[0]}!</h1>}
+
+        {/* User Level Indicator */}
+        <div className="user-level-indicator">
+          {levelColors.map((color, index) => (
+            <div key={color} className={`level-section ${color}`} style={{ width: userLevelIndex > index ? '25%' : '0%' }}></div>
+          ))}
+        </div>
+
+        {/* Todays Date */}
+        <p className="current-date">Dato: {date}</p>
+
       </div>
     </div>
   );
