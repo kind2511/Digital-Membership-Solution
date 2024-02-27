e0eec39a3646f41c1f4ca2b65a473b253e6e79b4
Jorgen Johnsen <jamjohns@stud.ntnu.no>
2024-02-27 11:21:44 +0100

    fixed backend errors

--- gnist/BackEnd/gnist/gnist/settings.py
+++ gnist/BackEnd/gnist/gnist/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,27 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    "mysite",
-
-    "social_django"
 ]
-
-SOCIAL_AUTH_TRAILING_SLASH=False
-SOCIAL_AUTH_AUTH0_DOMAIN=config('APP_DOMAIN')
-SOCIAL_AUTH_AUTH0_KEY=config('APP_CLIENT_ID')
-SOCIAL_AUTH_AUTH0_SECRET=config('APP_CLIENT_SECRET')
-
-SOCIAL_AUTH_AUTH0_SCOPE=[
-    'openid',
-    'profile',
-    'email'
-]
-
-AUTHENTICATION_BACKENDS={
-    'social_core.backends.auth0.Auth0OAuth2',
-    'django.contrib.auth.backends.ModelBackend'
-}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -153,11 +132,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-LOGIN_URL="/login/auth0"
-LOGIN_REDIRECT_URL="/"
-LOGIN_REDIRECT_URL="/"
-
-AUTH_USER_MODEL = 'mysite.CustomUser'
-
--- gnist/BackEnd/gnist/gnist/urls.py
+++ gnist/BackEnd/gnist/gnist/urls.py
@@ -20,5 +20,4 @@ from django.urls import include, path
 urlpatterns = [
     path("digital_medlemsordning/", include("digital_medlemsordning.urls")),
     path("admin/", admin.site.urls),
-    path('',include('mysite.urls'))
 ]
