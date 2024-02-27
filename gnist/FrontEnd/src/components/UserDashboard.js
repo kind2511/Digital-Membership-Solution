9f0c985ed436c25f3996cf43763e4cf38a62e41e
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-27 15:13:41 +0100

    Fix logout and add function to lock current page

--- gnist/FrontEnd/src/components/UserDashboard.js
+++ gnist/FrontEnd/src/components/UserDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import './UserDashboard.css';
 
@@ -8,6 +8,14 @@ function UserDashboard() {
   const handleLogout = () => {
     logout({ returnTo: window.location.origin });
   };
+
+  useEffect(() => {
+    // "Lock" the current page in the browser's history
+    window.history.pushState(null, null, window.location.href);
+    window.onpopstate = function () {
+      window.history.go(1);
+    };
+  }, []);
 
   return (
     <div className="dashboard-container">
@@ -21,11 +29,11 @@ function UserDashboard() {
           <div className="sidebar-item">Mine Aktiviteter</div>
           <div className="sidebar-item">Meldinger</div>
           <div className="sidebar-item">Hjelp</div>
-          <div className="sidebar-item" onClick={handleLogout}>Logg Ut</div>
+          <div className="sidebar-item logout-item" onClick={handleLogout}>Logg Ut</div>
         </div>
       </div>
       <div className="main-content">
-        {/* will be added later */}
+        {/* Content will be added later */}
       </div>
     </div>
   );
