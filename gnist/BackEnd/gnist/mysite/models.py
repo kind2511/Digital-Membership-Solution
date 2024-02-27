852c354f21a67e2dc1e3c27d3f3052058e419dee
ahmadmm <ahmadmm@stud.ntnu.no>
2024-02-27 10:59:10 +0100

    Remove unnecessary files

--- gnist/BackEnd/gnist/mysite/models.py
+++
@@ -1,8 +0,0 @@
-# mysite/models.py
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-
