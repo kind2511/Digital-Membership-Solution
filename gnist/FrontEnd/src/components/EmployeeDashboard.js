339dc3b427aee6b043166107c5db6879caf701c9
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-16 16:26:39 +0100

    Add EmployeeDashboard component

---
+++ gnist/FrontEnd/src/components/EmployeeDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EmployeeDashboard.css';  
+
+function EmployeeDashboard() {
+  return (
+    <div className="employee-dashboard">
+      <h1>Employee Dashboard</h1>
+    </div>
+  );
+}
+
+export default EmployeeDashboard;
