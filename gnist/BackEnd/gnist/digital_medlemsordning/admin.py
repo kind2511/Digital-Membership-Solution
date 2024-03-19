58a9b1457613a750351847dcb39b538acca0f44a
kind2511 <chrisa2511@gmail.com>
2024-03-19 20:44:15 +0100

    added PollQuestion and PollAnwer models to admin registery

--- gnist/BackEnd/gnist/digital_medlemsordning/admin.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/admin.py
@@ -8,6 +8,8 @@ from .models import ActivitySignup
 from .models import SuggestionBox
 from .models import Level
 from .models import Message
+from .models import PollQuestion
+from .models import PollAnswer
 
 
 # Register your models here.
@@ -20,3 +22,6 @@ admin.site.register(ActivitySignup)
 admin.site.register(SuggestionBox)
 admin.site.register(Level)
 admin.site.register(Message)
+admin.site.register(PollQuestion)
+admin.site.register(PollAnswer)
+
