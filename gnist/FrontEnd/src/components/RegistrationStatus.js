140212656c4050c0704dfa395dee46832329e444
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-24 17:22:09 +0100

    Remove the confirmation modal for register user with its messages

--- gnist/FrontEnd/src/components/RegistrationStatus.js
+++ gnist/FrontEnd/src/components/RegistrationStatus.js
@@ -5,28 +5,20 @@ function RegistrationStatus({ userSub, isRegistered, setIsRegistered }) {
   const baseApiUrl = 'http://localhost:8000';
 
   useEffect(() => {
-    // Fetch the registration status from localStorage on component mount
     const storedRegistrationStatus = localStorage.getItem('isRegistered') === 'true';
     setIsRegistered(storedRegistrationStatus);
   }, [setIsRegistered]);
 
   const toggleRegistration = async () => {
-    // Ask for confirmation before toggling registration status
-    const confirmMessage = isRegistered
-      ? "Er du sikker på at du vil avregistrere deg?"
-      : "Er du sikker på at du vil registrere deg?";
+    const endpoint = `${baseApiUrl}/digital_medlemsordning/add_day/${userSub}/`;
 
-    if (window.confirm(confirmMessage)) {
-      const endpoint = `${baseApiUrl}/digital_medlemsordning/add_day/${userSub}/`;
-
-      try {
-        await axios.get(endpoint); 
-        setIsRegistered(!isRegistered);
-        localStorage.setItem('isRegistered', !isRegistered);
-        console.log(!isRegistered ? 'User registered.' : 'User unregistered.');
-      } catch (error) {
-        console.error("Error toggling registration:", error.response || error);
-      }
+    try {
+      await axios.get(endpoint); 
+      setIsRegistered(!isRegistered);
+      localStorage.setItem('isRegistered', !isRegistered);
+      console.log(!isRegistered ? 'User registered.' : 'User unregistered.');
+    } catch (error) {
+      console.error("Error toggling registration:", error.response || error);
     }
   };
 
