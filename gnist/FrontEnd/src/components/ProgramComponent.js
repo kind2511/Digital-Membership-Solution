e6508c30582b13f338c46ae37fbc9541899d6447
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-24 21:13:05 +0100

    Fixed signUp activity in program component

--- gnist/FrontEnd/src/components/ProgramComponent.js
+++ gnist/FrontEnd/src/components/ProgramComponent.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
 import './ProgramComponent.css';
 
 function ProgramComponent() {
+  const { user, isAuthenticated } = useAuth0(); 
   const [programs, setPrograms] = useState([]);
   const [selectedProgram, setSelectedProgram] = useState(null);
+  const [signupStatus, setSignupStatus] = useState('');
+  const baseApiUrl = 'http://127.0.0.1:8000';
 
   useEffect(() => {
     fetchActivities();
@@ -11,7 +16,7 @@ function ProgramComponent() {
 
   const fetchActivities = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/digital_medlemsordning/get_all_activity/');
+      const response = await fetch(`${baseApiUrl}/digital_medlemsordning/get_all_activity/`);
       if (!response.ok) {
         throw new Error('Failed to fetch activities');
       }
@@ -26,26 +31,59 @@ function ProgramComponent() {
     setSelectedProgram(program);
   };
 
+  const handleSignUp = async (activityId) => {
+    if (!isAuthenticated) {
+      console.log('User is not authenticated');
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${baseApiUrl}/digital_medlemsordning/sign_up_activity/`, {
+        auth0_id: user.sub, 
+        activity_id: activityId,
+      });
+
+      if (response.status === 200 || response.status === 201) {
+        setSignupStatus('Du er registrert nå.');
+        setTimeout(() => {
+          setSignupStatus('');
+          setSelectedProgram(null); 
+        }, 3000);
+      }
+    } catch (error) {
+      setSignupStatus('Du kan ikke registrere nå, vennligst vent litt.');
+      setTimeout(() => {
+        setSignupStatus('');
+      }, 3000);
+      console.error('Failed to sign up for the activity:', error);
+    }
+  };
+
   return (
     <div className="program-list">
       {programs.map(program => (
         <div key={program.title} className="program-entry" onClick={() => handleTitleClick(program)}>
-          <img src={program.image ? program.image : "https://via.placeholder.com/60"} alt="Program" className="program-image" />
+          <img src={program.image ? program.image : "https://via.placeholder.com/60"} alt={program.title} className="program-image" />
           <div className="program-details">
             <div className="program-title">{program.title}</div>
             <div className="program-date">{program.dates.join(", ")}</div>
           </div>
         </div>
       ))}
+
+      
       {selectedProgram && (
         <div className="program-modal" onClick={() => setSelectedProgram(null)}>
           <div className="program-modal-content" onClick={e => e.stopPropagation()}>
             <h2>{selectedProgram.title}</h2>
             <p>{selectedProgram.description}</p>
             <div className="modal-buttons">
-              <button onClick={() => setSelectedProgram(null)}>Close</button>
-              <button>{selectedProgram.sign_up ? "Meld av" : "Meld på"}</button>
+              <button onClick={() => setSelectedProgram(null)}>Lukk</button>
+              <button onClick={() => handleSignUp(selectedProgram.activity_id)}>
+                {selectedProgram.sign_up ? "Meld av" : "Meld på"}
+              </button>
             </div>
+            {signupStatus && <div className="signup-status">{signupStatus}</div>}
           </div>
         </div>
       )}
