c86acafcddf555aa7d4b6beead27b2bdd960f446
ivemm <farah_ive@yahoo.com>
2024-03-25 20:29:13 +0100

    Create MinMening function

---
+++ gnist/FrontEnd/src/components/MinMening.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './MinMening.css';
+
+function MinMening() {
+   return (
+    <div className="minMening-container">
+    </div>
+
+   );
+
+}
+
+export default MinMening;
