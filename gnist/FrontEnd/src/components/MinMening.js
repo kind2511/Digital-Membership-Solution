78dbff3add5d21347d277d7cdf47c249a56a29de
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-25 20:53:28 +0100

    Add send inn forslag title

--- gnist/FrontEnd/src/components/MinMening.js
+++ gnist/FrontEnd/src/components/MinMening.js
@@ -5,6 +5,7 @@ function MinMening() {
    return (
     <div className="minMening-container">
       <hr className="divider" />
+      <h1 className="title">Send Inn Forslag</h1>
     </div>
    );
 }
