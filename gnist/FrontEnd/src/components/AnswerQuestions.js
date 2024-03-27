8f3c8be3276d7bd034a7fc8fc00d063eee337b40
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-27 15:41:53 +0100

    Add new component AnswerQuestions

---
+++ gnist/FrontEnd/src/components/AnswerQuestions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AnswerQuestions.css';
+
+function AnswerQuestions() {
+
+    return (
+        <div>
+            <h1> details will be here for this new component</h1>
+        </div>
+    );
+}
+
+export default AnswerQuestions;
