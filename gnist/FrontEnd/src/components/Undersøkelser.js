ce24528efad613ab5edc0ada05871ee8a0167455
ahmadmm <ahmadmm@stud.ntnu.no>
2024-03-31 19:36:03 +0200

    Add Undersøkelser component

---
+++ gnist/FrontEnd/src/components/Undersøkelser.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Undersøkelser.css';
+
+function Undersøkelser() {
+  return <div className="undersøkelser">Undersøkelser</div>;
+}
+
+export default Undersøkelser;
