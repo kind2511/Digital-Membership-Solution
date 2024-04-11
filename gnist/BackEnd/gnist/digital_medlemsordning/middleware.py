83175fdd36e014d90ded4651ae533d4dceaebf70
kind2511 <chrisa2511@gmail.com>
2024-04-11 17:04:38 +0200

    commented out defunct middlware

--- gnist/BackEnd/gnist/digital_medlemsordning/middleware.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/middleware.py
@@ -1,26 +1,26 @@
 from datetime import date, timedelta
 from .models import Members
 
-# Middleware that unbannes a member once the banned period has expired
-class UnbannedMemberMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# # Middleware that unbannes a member once the banned period has expired
+# class UnbannedMemberMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        response = self.get_response(request)
+#     def __call__(self, request):
+#         response = self.get_response(request)
 
-    # Check if any members are currently banned
-        banned_members = Members.objects.filter(banned=True)
+#     # Check if any members are currently banned
+#         banned_members = Members.objects.filter(banned=True)
     
-     # Iterate over banned members and update their banned status if ban has expired
-        for member in banned_members:
-            if member.banned_until <= date.today():
-                member.banned = False
-                member.banned_from = None
-                member.banned_until = None
-                member.save()
+#      # Iterate over banned members and update their banned status if ban has expired
+#         for member in banned_members:
+#             if member.banned_until <= date.today():
+#                 member.banned = False
+#                 member.banned_from = None
+#                 member.banned_until = None
+#                 member.save()
 
-        return response
+#         return response
     
 
 # Middleware that removes guardian information (name and phonenumber) when a member turns 18
