4e5d311111727dc54326ccc0d11bed9079e14181
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-16 12:48:56 +0200

    Refactor redirection logic, integrate registration status check

--- gnist/FrontEnd/src/components/HomePageWithRedirection.js
+++ gnist/FrontEnd/src/components/HomePageWithRedirection.js
@@ -1,31 +1,49 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import HomePage from './HomePage';
-import Loading from './Loading';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import HomePage from "./HomePage";
+import Loading from "./Loading";
 
 function HomePageWithRedirection() {
   const { isAuthenticated, user, isLoading } = useAuth0();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLoading && isAuthenticated) {
-    
-      const userProfileComplete = false; 
+    const fetchData = async () => {
+      // check that user is authenticated
+      if (!isLoading && isAuthenticated && user) {
+        try {
+          // check if role is employee
+          const roles = user["https://digitalmedlemsordning/roles"];
+          const isEmployee = roles.includes("employee");
 
-      // Extract the role from the user's data
-      const role = user['https://my-namespace/role'];
+          if (isEmployee) {
+            // send employee to employee dashboard
+            navigate("/employee-dashboard");
+          } else {
+            // For non-employees, check registration status
+            const sub = user.sub; // Get the sub from auth0
+            const response = await fetch(
+              `http://127.0.0.1:8000/digital_medlemsordning/check_user_registration_status/?sub=${sub}`
+            );
+            const data = await response.json();
 
-      // Redirect based on the user's role and profile completeness
-      if (!userProfileComplete) {
-        navigate('/user-info');
-      } else if (role === 'admin') {
-        navigate('/admin-dashboard');
-      } else {
-        navigate('/user-dashboard');
+            if (data.registered) {
+              // if fully registered
+              navigate("/user-dashboard");
+            } else {
+              // if not fully registered
+              navigate("/user-info");
+            }
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
       }
-    }
-  }, [isAuthenticated, isLoading, navigate, user]);
+    };
+
+    fetchData();
+  }, [isAuthenticated, isLoading, user, navigate]);
 
   return isLoading ? <Loading /> : <HomePage />;
 }
