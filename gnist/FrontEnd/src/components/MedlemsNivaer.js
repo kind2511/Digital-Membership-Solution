dd72a13e455b42e6b6fd887adb9e42999432b481
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-18 15:52:18 +0200

    add new button for add new level then change some class names

--- gnist/FrontEnd/src/components/MedlemsNivaer.js
+++ gnist/FrontEnd/src/components/MedlemsNivaer.js
@@ -33,19 +33,20 @@ function MedlemsNivaer() {
 
     return (
         <div className="medlems-nivaer-container">
-            <h2 className="section-title">Medlems Nivåer</h2>
+            <h2 className="medlems-nivaer-section-title">Medlems Nivåer</h2>
             <div className="section-content">
                 <div className="level-list">
                     {levels.map((level, index) => (
                         <div key={index} className="level-item">
                             <p className="level-info">{level.name} - {level.points} Points</p>
                             <div>
-                                <button className="level-button edit-button" onClick={() => handleEditLevel(level.levelID)}>Edit</button>
-                                <button className="level-button delete-button" onClick={() => handleDeleteLevel(level.levelID)}>Delete</button>
+                                <button className="level-button edit-button" onClick={() => handleEditLevel(level.levelID)}>Redigere</button>
+                                <button className="level-button delete-button" onClick={() => handleDeleteLevel(level.levelID)}>Slett</button>
                             </div>
                         </div>
                     ))}
                 </div>
+                <button className="level-button add-button" onClick={() => console.log('Adding new level')}>Legg til nytt nivå</button>
             </div>
         </div>
     );
