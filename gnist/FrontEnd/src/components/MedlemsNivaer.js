958b4f59c90e58176369e0e4e49d26b892458a7d
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-18 16:15:50 +0200

    Add success message for adding level

--- gnist/FrontEnd/src/components/MedlemsNivaer.js
+++ gnist/FrontEnd/src/components/MedlemsNivaer.js
@@ -5,6 +5,7 @@ function MedlemsNivaer() {
     const [levels, setLevels] = useState([]);
     const [newLevelName, setNewLevelName] = useState('');
     const [newLevelPoints, setNewLevelPoints] = useState('');
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
     useEffect(() => {
         fetchLevels();
@@ -48,9 +49,13 @@ function MedlemsNivaer() {
             if (!response.ok) {
                 throw new Error('Failed to add new level');
             }
+            setShowSuccessMessage(true);
             setNewLevelName('');
             setNewLevelPoints('');
             fetchLevels(); // Fetch levels again to update the list
+            setTimeout(() => {
+                setShowSuccessMessage(false); // Hide the success message after 3 seconds
+            }, 3000);
         } catch (error) {
             console.error(error);
         }
@@ -63,7 +68,7 @@ function MedlemsNivaer() {
                 <div className="level-list">
                     {levels.map((level, index) => (
                         <div key={index} className="level-item">
-                            <p className="level-info">{level.name} - {level.points} Points</p>
+                            <p className="level-info">{level.name} - {level.points} Poeng</p>
                             <div>
                                 <button className="level-button edit-button" onClick={() => handleEditLevel(level.levelID)}>Redigere</button>
                                 <button className="level-button delete-button" onClick={() => handleDeleteLevel(level.levelID)}>Slett</button>
@@ -87,6 +92,11 @@ function MedlemsNivaer() {
                     <button className="level-button add-button" onClick={handleAddLevel}>Legg til nytt nivå</button>
                 </div>
             </div>
+            {showSuccessMessage && (
+                <div className="success-banner">
+                    Niva har blitt lagt til
+                </div>
+            )}
         </div>
     );
 }
