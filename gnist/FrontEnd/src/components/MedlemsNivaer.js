5104506f722d24c28691ed7634d5f6c6788f0d60
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-18 15:26:44 +0200

    Add style for the memeber levels components

--- gnist/FrontEnd/src/components/MedlemsNivaer.js
+++ gnist/FrontEnd/src/components/MedlemsNivaer.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MedlemsNivaer.css';
 
 function MedlemsNivaer() {
+    const [levels, setLevels] = useState([]);
+
+    useEffect(() => {
+        fetchLevels();
+    }, []);
+
+    const fetchLevels = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/digital_medlemsordning/get_all_levels/');
+            if (!response.ok) {
+                throw new Error('Failed to fetch levels');
+            }
+            const data = await response.json();
+            setLevels(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleEditLevel = (levelID) => {
+        //TODO
+        console.log(`Editing level with ID: ${levelID}`);
+    };
+
+    const handleDeleteLevel = (levelID) => {
+        //TODO
+        console.log(`Deleting level with ID: ${levelID}`);
+    };
+
     return (
         <div className="medlems-nivaer-container">
             <h2 className="section-title">Medlems Nivåer</h2>
-            <div className="section-content"></div>
+            <div className="section-content">
+                <div className="level-list">
+                    {levels.map((level, index) => (
+                        <div key={index} className="level-item">
+                            <p className="level-info">{level.name} - {level.points} Points</p>
+                            <div>
+                                <button className="level-button edit-button" onClick={() => handleEditLevel(level.levelID)}>Edit</button>
+                                <button className="level-button delete-button" onClick={() => handleDeleteLevel(level.levelID)}>Delete</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
