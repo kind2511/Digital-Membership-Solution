7a0293c151b501fe28bf1e5c1e17c5554a59eb18
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-18 20:28:44 +0200

    Apply get all un verified users function with its style

--- gnist/FrontEnd/src/components/EndeligGodkjenning.js
+++ gnist/FrontEnd/src/components/EndeligGodkjenning.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './EndeligGodkjenning.css';
 
 function EndeligGodkjenning() {
+    const [unverifiedMembers, setUnverifiedMembers] = useState([]);
+
+    useEffect(() => {
+        fetchUnverifiedMembers();
+    }, []);
+
+    const fetchUnverifiedMembers = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/digital_medlemsordning/get_all_unverified_members/');
+            if (!response.ok) {
+                throw new Error('Failed to fetch unverified members');
+            }
+            const data = await response.json();
+            setUnverifiedMembers(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <div className="endelig-godkjenning-container">
-            <h2 className="section-title">Endelig Godkjenning</h2>
-            <div className="section-content"></div>
+        <div className="eg-godkjenning-container">
+            <h2 className="eg-section-title">Endelig Godkjenning</h2>
+            <div className="eg-section-content">
+                <ul className="eg-members-list">
+                    {unverifiedMembers.map(member => (
+                        <li key={member.id} onClick={() => console.log(`Member ${member.first_name} ${member.last_name} clicked`)}>
+                            {member.first_name} {member.last_name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
