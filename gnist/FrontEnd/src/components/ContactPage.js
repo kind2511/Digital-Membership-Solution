ccf60c11583e14effbb5c14ddb1577167aac749b
ahmadmm <ahmadmm@stud.ntnu.no>
2024-04-24 22:01:41 +0200

    Add additional information link and adjust styling on Contact page

--- gnist/FrontEnd/src/components/ContactPage.js
+++ gnist/FrontEnd/src/components/ContactPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ContactPage.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSnapchatGhost, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 const ContactPage = () => {
   return (
@@ -8,19 +10,24 @@ const ContactPage = () => {
         <h1>Kontakt oss</h1>
         <div className="social-media-links">
           <a href="https://t.snapchat.com/MzMp6E37" target="_blank" rel="noopener noreferrer">
-            <img src="/Snapchat_App_Icon.png" alt="Snapchat" />
+            <FontAwesomeIcon icon={faSnapchatGhost} size="2x" />
           </a>
           <a href="https://www.instagram.com/fyrung?igshid=ZDBsbW15ZGVkcDY2" target="_blank" rel="noopener noreferrer">
-            <img src="/Instagram_App_Icon.png" alt="Instagram" />
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
           </a>
           <a href="https://www.tiktok.com/@fyrung" target="_blank" rel="noopener noreferrer">
-            <img src="/Tiktok-App_Icon.png" alt="TikTok" />
+            <FontAwesomeIcon icon={faTiktok} size="2x" />
           </a>
         </div>
-        <p>Trenger du å snakke med noen? Send melding til 91 91 74 10 (Kathrine)</p>
-        <p>Har du et forslag til klubben? Send inn her: 
-          <a href="mailto:fyrverkeriet.ungdomshus@gmail.com">fyrverkeriet.ungdomshus@gmail.com</a>, hvis mulig.
-        </p>
+        <div className="contact-info">
+          <p>Trenger du å snakke med noen? <span className="contact-detail">91 91 74 10 (Kathrine)</span></p>
+          <p>Har du et forslag til klubben? Send inn her: 
+            <a className="email-link" href="mailto:fyrverkeriet.ungdomshus@gmail.com">fyrverkeriet.ungdomshus@gmail.com</a>, hvis mulig.
+          </p>
+          <p>For mer info 
+            <a className="info-link" href="https://www.vestre-toten.kommune.no/fyrverkerietung/fyrverkerietungdomshus/" target="_blank" rel="noopener noreferrer">her</a>.
+          </p>
+        </div>
       </div>
     </div>
   );
