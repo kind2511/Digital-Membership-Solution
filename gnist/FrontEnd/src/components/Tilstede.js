8360fff8e30d2857cc98ecdbb874a8638bbe227a
kind2511 <chrisa2511@gmail.com>
2024-05-01 03:06:29 +0200

    removed percentage for statistics under less than 1%

--- gnist/FrontEnd/src/components/Tilstede.js
+++ gnist/FrontEnd/src/components/Tilstede.js
@@ -200,11 +200,17 @@ function Tilstede() {
                   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
                   const x = cx + radius * Math.cos(-midAngle * RADIAN);
                   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-                  return (
-                    <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
-                      {`${(percent * 100).toFixed(0)}%`}
-                    </text>
-                  );
+
+                  // Display percentage only if it's above 0%
+                  if (percent > 0) {
+                    return (
+                      <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+                        {`${(percent * 100).toFixed(0)}%`}
+                      </text>
+                    );
+                  } else {
+                    return null; // Return null if percentage is 0%
+                  }
                 }}
                 fill="#8884d8"
               />
