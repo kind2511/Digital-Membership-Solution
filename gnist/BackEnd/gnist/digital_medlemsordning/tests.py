99756742a8a0732f135a9b9072d6c7fc2fb13d39
kind2511 <chrisa2511@gmail.com>
2024-05-07 23:14:18 +0200

    created tests for getting todays activites

--- gnist/BackEnd/gnist/digital_medlemsordning/tests.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/tests.py
@@ -20,6 +20,7 @@ class GetFutureActivitiesTestCase(APITestCase):
     def test_get_future_activities(self):
         url = reverse('get_future_activities')
         response = self.client.get(url)
+        # Test correct statuscode
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         # Get expected data of only activites with a date of today or in the future
@@ -47,9 +48,10 @@ class GetPastActivitiesTestCase(APITestCase):
         Activity.objects.create(title="Future Activity", description="Description 3", date=today + timedelta(days=1))
         Activity.objects.create(title="Past Activity", description="Description 4", date=today - timedelta(days=1))
 
-    def test_get_future_activities(self):
+    def test_past_future_activities(self):
         url = reverse('get_past_activities')
         response = self.client.get(url)
+        # Test correct status code
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         # Get expected data of only activites that has already occured
@@ -59,10 +61,41 @@ class GetPastActivitiesTestCase(APITestCase):
         
         self.assertEqual(response.data, expected_data)
 
-    def test_get_future_activities_no_data(self):
+    def test_get_past_activities_no_data(self):
         # Test when there are no past activities
         url = reverse('get_past_activities')
         Activity.objects.filter(date__lt=date.today()).delete()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, [])
+
+
+class GetTodaysActivitiesTestCase(APITestCase):
+    def setUp(self):
+        # Create test data
+        today = date.today()
+        Activity.objects.create(title="Activity 1", description="Description 1", date=today)
+        Activity.objects.create(title="Activity 2", description="Description 2", date=today)
+        Activity.objects.create(title="Future Activity", description="Description 3", date=today + timedelta(days=1))
+        Activity.objects.create(title="Past Activity", description="Description 4", date=today - timedelta(days=1))
+
+    def test_get_todays_activities(self):
+        url = reverse('get_activity_today')
+        response = self.client.get(url)
+        # Test correct status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # Get expected data of only todays activites
+        today = date.today()
+        future_activities = Activity.objects.filter(date=today)
+        expected_data = ActivitySerializer(future_activities, many=True).data
+        
+        self.assertEqual(response.data, expected_data)
+
+    def test_get_todays_activities_no_data(self):
+        # Test when there are no activities for today
+        url = reverse('get_activity_today')
+        Activity.objects.filter(date=date.today()).delete()
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, [])
