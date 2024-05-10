fca636828c6f2b63ea5b1db86ceeee9e7edd14ed
kind2511 <chrisa2511@gmail.com>
2024-05-10 19:13:52 +0200

    added test for delting activity

--- gnist/BackEnd/gnist/digital_medlemsordning/tests.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from datetime import date, timedelta
 from django.urls import reverse
 from rest_framework import status
@@ -141,26 +141,66 @@ class GetAllActivitiesTestCase(APITestCase):
         self.assertEqual(response.data, [])
 
 
-class CreateActivityTestCase(APITestCase):
-    def test_create_activity_invalid_data(self):
-        url = reverse('create_activity') 
-        invalid_data = {'title': '', 'description': 'Description of the new activity'}
-        
-        # Make a POST request with invalid data
-        response = self.client.post(url, invalid_data, format='json')
-        
-        # Check that the status code is 400 (Bad Request)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-        # Check that the response contains errors
-        self.assertIn('title', response.data)  # Assuming 'title' is a required field
+class DeleteActivityTestCase(APITestCase):
+    def setUp(self):
+        self.activity = Activity.objects.create(
+            activityID=1,
+            title='Test Activity',
+            description='Test description',
+            image='test_image.jpg',
+            date='2024-05-10',
+            limit=10,
+            signed_up_count=0
+        )
+        self.url = reverse('delete_activity', kwargs={'activity_id': self.activity.activityID})
 
-    def test_create_activity_invalid_method(self):
-        url = reverse('create_activity') 
-        
-        # Make a GET request instead of POST
-        response = self.client.get(url)
-        
-        # Check that the status code is 405 (Method Not Allowed)
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_delete_activity_success(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # Check if the activity is deleted from the database
+        self.assertFalse(Activity.objects.filter(activityID=self.activity.activityID).exists())
+
+    def test_delete_activity_not_found(self):
+        non_existent_url = reverse('delete_activity', kwargs={'activity_id': 999})
+        response = self.client.delete(non_existent_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['error'], 'Activity not found')
+
+
+# class CreateActivityTestCase(APITestCase):
+#     def setUp(self):
+#         self.url = reverse('create_activity')
+#         self.client = Client()  # Use Django's test client for form data
+#         self.valid_payload = {
+#             'title': 'New Activity',
+#             'description': 'Description of the activity',
+#             'image': '../media/activity_pics/81zm9tKLsxL._AC_SL1170__0ncFLPi.jpg',
+#             'date': '2024-05-10',
+#             'limit': 10,
+#         }
+
+#     def test_create_activity_success(self):
+#         response = self.client.post(self.url, self.valid_payload, format='multipart')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+#          # Print the response data for debugging
+#         print("Response data:", response.data)
+
+#         # Check the response data matches the valid payload
+#         self.assertEqual(response.data['title'], self.valid_payload['title'])
+#         self.assertEqual(response.data['description'], self.valid_payload['description'])
+#         self.assertEqual(response.data['image'], self.valid_payload['image'])
+#         self.assertEqual(response.data['date'], self.valid_payload['date'])
+#         self.assertEqual(response.data['limit'], self.valid_payload['limit'])
+
+#     def test_create_activity_invalid_data(self):
+#         invalid_payload = self.valid_payload.copy()
+#         invalid_payload.pop('title')  # Removing required field
+#         response = self.client.post(self.url, invalid_payload, format='multipart')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+#     def test_create_activity_invalid_method(self):
+#         response = self.client.get(self.url)
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
