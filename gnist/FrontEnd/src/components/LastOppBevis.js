16c62a9cae02c5073b3ddb211a6b5f186950529d
ahmadmm <ahmadmm@stud.ntnu.no>
2024-05-13 20:32:42 +0200

    Add function to show saved bevis for specifc memeber

--- gnist/FrontEnd/src/components/LastOppBevis.js
+++ gnist/FrontEnd/src/components/LastOppBevis.js
@@ -7,7 +7,9 @@ function LastOppBevis() {
     const [results, setResults] = useState([]);
     const [searchStatus, setSearchStatus] = useState('');
     const [uploadSuccess, setUploadSuccess] = useState(false);
-    const [showModal, setShowModal] = useState(false); 
+    const [showModal, setShowModal] = useState(false);
+    const [certificates, setCertificates] = useState([]);
+    const [imageViewer, setImageViewer] = useState('');
     const fileInputRef = useRef(null);
     const certificateNameRef = useRef(null);
 
@@ -19,19 +21,25 @@ function LastOppBevis() {
         }
         setSearchStatus('Searching...');
         fetch(`http://127.0.0.1:8000/digital_medlemsordning/search_member/?name=${value}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((data) => {
+            .then(response => response.json())
+            .then(data => {
                 setResults(data);
                 setSearchStatus(`Found ${data.length} results.`);
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error("Error fetching data:", error);
                 setSearchStatus("Failed to fetch data. Please try again later.");
+            });
+    };
+
+    const fetchCertificates = (auth0ID) => {
+        fetch(`http://127.0.0.1:8000/digital_medlemsordning/get_member_certificates/${auth0ID}/`)
+            .then(response => response.json())
+            .then(data => {
+                setCertificates(data);
+            })
+            .catch(error => {
+                console.error("Error fetching certificates:", error);
             });
     };
 
@@ -46,6 +54,7 @@ function LastOppBevis() {
         setSearchTerm(`${member.first_name} ${member.last_name}`);
         setResults([]);
         setSearchStatus('');
+        fetchCertificates(member.auth0ID);
     };
 
     const handleUpload = () => {
@@ -56,48 +65,55 @@ function LastOppBevis() {
         const file = fileInputRef.current.files[0];
         const certificateName = certificateNameRef.current.value;
         if (!file || !certificateName) {
-            alert('Velg en fil og skriv inn et bevis navn for å laste opp..');
+            alert('Velg en fil og skriv inn et bevis navn for å laste opp.');
             return;
         }
         const formData = new FormData();
         formData.append('certificate_image', file);
-        formData.append('certificate_name', certificateName); //manually entered certificate name
-    
+        formData.append('certificate_name', certificateName);
+
         fetch(`http://127.0.0.1:8000/digital_medlemsordning/upload_member_certificates/${selectedMember.auth0ID}/`, {
-            method: 'POST', 
+            method: 'POST',
             body: formData,
             headers: {
                 'Accept': 'application/json',
             },
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            return response.json();  
-        })
-        .then(data => {
-            console.log('Success:', data);
-            setUploadSuccess(true);
-            setTimeout(() => setUploadSuccess(false), 3000);
-            setSelectedMember(null);
-            setSearchTerm('');
-        })
-        .catch(error => {
-            alert('Failed to upload certificate.');
-            console.error('Error:', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+                setUploadSuccess(true);
+                setTimeout(() => setUploadSuccess(false), 3000);
+                setSelectedMember(null);
+                setSearchTerm('');
+                setCertificates([]); 
+            })
+            .catch(error => {
+                alert('Failed to upload certificate.');
+                console.error('Error:', error);
+            });
     };
 
     const handleClose = () => {
         setSelectedMember(null);
         setSearchTerm('');
         setSearchStatus('');
+        setCertificates([]); // Clear certificates on close
+        setImageViewer(''); // Clear the image viewer
     };
 
-    // Function to toggle modal display
     const toggleModal = () => {
         setShowModal(!showModal);
+        setImageViewer(''); 
+    };
+
+    const handleCertificateClick = (cert) => {
+        setImageViewer(cert.certificate_image); 
     };
 
     return (
@@ -117,11 +133,10 @@ function LastOppBevis() {
                 <div className="selected-member">
                     <div className="selected-name">{`${selectedMember.first_name} ${selectedMember.last_name}`}</div>
                     <input type="file" ref={fileInputRef} className="file-input" />
-                    <input type="text" ref={certificateNameRef} placeholder="Skriv inn bevis navn" className="certificate-name-input"/>
+                    <input type="text" ref={certificateNameRef} placeholder="Skriv inn bevis navn" className="certificate-name-input" />
                     <div className="buttons-container">
                         <button className="upload-button" onClick={handleUpload}>Last Opp</button>
                         <button className="close-button" onClick={handleClose}>Lukk</button>
-                        {/* vis bevis */}
                         <button className="vis-bevis-modal-button" onClick={toggleModal}>Vis Andre Bevis</button>
                     </div>
                 </div>
@@ -137,12 +152,18 @@ function LastOppBevis() {
             {uploadSuccess && (
                 <div className="last-opp-success-message">Bevis ble lastet opp</div>
             )}
-            {/* Modal container */}
             {showModal && (
                 <div className="last-opp-modal-container">
                     <div className="last-opp-modal-content">
                         <span className="last-opp-close-modal" onClick={toggleModal}>&times;</span>
-                        <p>Modalt innhold går her.</p>
+                        <div className="certificates-container">
+                            {certificates.map((cert, index) => (
+                                <div key={index} className="certificate-entry" onClick={() => handleCertificateClick(cert)}>
+                                    {`${index + 1}. ${cert.certificate_name}`}
+                                </div>
+                            ))}
+                        </div>
+                        {imageViewer && <img src={imageViewer} alt="Certificate View" className="certificate-image" />}
                     </div>
                 </div>
             )}
