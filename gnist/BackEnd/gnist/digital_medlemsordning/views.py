af4a1505ef39e7d61c65602543757f9361713ae5
kind2511 <chrisa2511@gmail.com>
2024-05-13 21:50:23 +0200

    added tests for deleting a question

--- gnist/BackEnd/gnist/digital_medlemsordning/views.py
+++ gnist/BackEnd/gnist/digital_medlemsordning/views.py
@@ -188,30 +188,6 @@ def submit_user_response(request, auth0_id):
     return Response({"message": "User response submitted successfully."}, status=201)
 
 
-# Gets all anwesers and correspoinding answer alternatives
-@api_view(['GET'])
-def get_all_questions_with_answers(request):
-    questions = PollQuestion.objects.all()
-    serialized_data = []
-
-    for question in questions:
-        serialized_question = PollQuestionSerializer(question).data
-        answers_data = []
-        
-        # Loop through answers for the current question
-        for answer in question.answers.all():
-            answer_data = {
-                'answer_id': answer.answerID,
-                'answer_text': answer.answer
-            }
-            answers_data.append(answer_data)
-        
-        serialized_question['answers'] = answers_data
-        serialized_data.append(serialized_question)
-
-    return Response({'questions': serialized_data}, status=200)
-
-
 # Gets the number of answers for each alternative answer for a specific question
 @api_view(['GET'])
 def get_answer_counts_for_question(request, question_id):
@@ -233,19 +209,6 @@ def get_answer_counts_for_question(request, question_id):
         "answer_counts": answer_counts
     }, status=200)
 
-
-# Deletes a specific question
-@api_view(['DELETE'])
-def delete_question(request, question_id):
-    try:
-        question = PollQuestion.objects.get(questionID=question_id)
-    except PollQuestion.DoesNotExist:
-        return Response({"error": "Question not found"}, status=404)
-
-    # Delete the question
-    question.delete()
-
-    return Response({"message": "Question deleted successfully"}, status=204)
 
 #-------------------------------------------------------------------------------------------------------------------------------
 
@@ -858,7 +821,7 @@ def delete_suggestion(request, suggestion_id):
 #-------------------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------------------
-# Suggestions
+# Questions and answers
 #-------------------------------------------------------------------------------------------------------
 
 # Create a question and corresponding possible answers
@@ -871,6 +834,42 @@ def create_question_with_answers(request):
     return Response({"message": "Could not create question."}, status=400)
 
 
+# Gets all anwesers and correspoinding answer alternatives
+@api_view(['GET'])
+def get_all_questions_with_answers(request):
+    questions = PollQuestion.objects.all()
+    serialized_data = []
+
+    for question in questions:
+        serialized_question = PollQuestionSerializer(question).data
+        answers_data = []
+        
+        # Loop through answers for the current question
+        for answer in question.answers.all():
+            answer_data = {
+                'answer_id': answer.answerID,
+                'answer_text': answer.answer
+            }
+            answers_data.append(answer_data)
+        
+        serialized_question['answers'] = answers_data
+        serialized_data.append(serialized_question)
+
+    return Response({'questions': serialized_data}, status=200)
+
+
+# Deletes a specific question
+@api_view(['DELETE'])
+def delete_question(request, question_id):
+    try:
+        question = PollQuestion.objects.get(questionID=question_id)
+    except PollQuestion.DoesNotExist:
+        return Response({"error": "Question not found"}, status=404)
+
+    # Delete the question
+    question.delete()
+
+    return Response({"message": "Question deleted successfully"}, status=204)
 #-------------------------------------------------------------------------------------------------------
 # Ban/Unban
 #-------------------------------------------------------------------------------------------------------
