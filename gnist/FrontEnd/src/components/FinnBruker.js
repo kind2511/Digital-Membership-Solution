f78325ed37fa41296290cae846898de182058905
ahmadmm <ahmadmm@stud.ntnu.no>
2024-05-14 11:18:59 +0200

    Fix query name from additional info to info

--- gnist/FrontEnd/src/components/FinnBruker.js
+++ gnist/FrontEnd/src/components/FinnBruker.js
@@ -76,7 +76,7 @@ function FinnBruker() {
                     <p>Verifisert: {selectedUser.verified ? 'Ja' : 'Nei'}</p>
                     <p>Status : {selectedUser.banned ? 'Ja' : 'Nei'}</p>
                     <p>Status Dato: {selectedUser.banned_from || 'N/A'} til {selectedUser.banned_until || 'N/A'}</p>
-                    <p>Ekstra Info: {selectedUser.additional_info || 'N/A'}</p>
+                    <p>Ekstra Info: {selectedUser.info || 'N/A'}</p>
                     <button className="close-details-button" onClick={handleCloseDetails}>Lukk</button>
                 </div>
             )}
